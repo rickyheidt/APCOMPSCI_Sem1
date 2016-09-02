b88e68708ce4de5fc33e9a39c5b4feb1901a2c72
Ricky Heidt <rgthree99@gmail.com>
2016-09-02 10:53:07 -0700

    Added Files

---
+++ APLesson_01/ACIIArt.java
@@ -0,0 +1,15 @@
+public class ACIIArt
+{
+	public static void main(String[]args)
+	{
+		System.out.println(" _____ _____ _____ _____ ");
+		System.out.println("|_   _|  ___/  ___|_   _|");
+		System.out.println("  | | | |__ \\ `--.  | |  ");
+		System.out.println("  | | |  __| `--. \\ | |  ");
+		System.out.println("  | | | |___/\\__/ / | |  ");
+		System.out.println("  \\_/ \\____/\\____/  \\_/  ");
+
+
+
+	}
+}
