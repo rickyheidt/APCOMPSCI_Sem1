9494094920b754a87235f5e2ae0b60895920774e
Ricky Heidt <rgthree99@gmail.com>
2016-09-02 11:00:54 -0700

    Added Files

---
+++ APLesson_01/secondprog.java
@@ -0,0 +1,8 @@
+public class secondprog
+{
+	public static void main(String[]args)
+	{
+		System.out.println("Hi Mr. Robinette");
+		System.out.println("The website said to make 2 programs so thats what i did here");
+	}
+}
