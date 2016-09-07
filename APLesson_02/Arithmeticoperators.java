c26518f3f462b77f35427de566973595ff95577b
Ricky Heidt <rgthree99@gmail.com>
2016-09-07 12:13:55 -0700

    added files

---
+++ APLesson_02/Arithmeticoperators.java
@@ -0,0 +1,11 @@
+public class Arithmeticoperators
+{
+	public static void main(String[]args)
+	{
+		System.out.println(8 + 5);
+		System.out.println(8 * 5);
+		System.out.println(8 - 5);
+		System.out.println(8 / 2);
+		System.out.println(50 % 100);
+	}
+}
---
+++ APLesson_02/Initializingvariables.java
@@ -0,0 +1,9 @@
+public class Initializingvariables
+{
+	public static void main(String[]args)
+	{
+		String name;
+		name = "Ricky";
+		System.out.println(name);
+	}
+}
