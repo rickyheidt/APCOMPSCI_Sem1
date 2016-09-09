65b04be41860096abd4bcd61693a7571b668b177
Ricky Heidt <rgthree99@gmail.com>
2016-09-09 12:02:48 -0700

    added files

---
+++ APLesson_02/Concatenation.java
@@ -0,0 +1,10 @@
+public class Concatenation
+{
+	public static void main(String[]args)
+	{
+		String first = "Professor";
+		String second = "Handsome";
+		
+		System.out.println(first + second);
+	}
+}
---
+++ APLesson_02/Lab_02.java
@@ -0,0 +1,31 @@
+public class Lab_02
+{
+	public static void main(String[]args)
+	{
+		int num1 = 2;
+		int num2 = 4;
+		int num3 = 5;
+		int num4 = 6;
+		int multi1 = num1 * num2;
+		int multi2 = num3 * num4;
+		System.out.println("2 muliplied by 4 is" + " " + multi1);
+		System.out.println("5 multiplied by 6 is" + " " + multi2);
+		
+		String address;
+		String name;
+		String city;
+		String zip;
+		zip = "92014";
+		city = "Del Mar, CA";
+		address = "288 dolphin cove court";
+		name = "Ricky Heidt";
+		System.out.println(name + " " + "lives at" + " " + address + " " + city + " " + zip);
+		
+		int l = 5;
+		int w = 10;
+		int h = 20;
+		int SA = 2*(w*l+h*l+h*w);
+		System.out.println("The surface area of my rectangle is " + SA);
+		
+	}
+}
