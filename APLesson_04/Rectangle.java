72211531135e7e38fad72c1eb3c1e16c9c51b20e
rickyheidt <rgthree99@gmail.com>
2016-10-03 11:46:19 -0700

    added files

--- APLesson_04/Rectangle.java
+++ APLesson_04/Rectangle.java
@@ -8,11 +8,21 @@ public class Rectangle
 	public static void main(String[]args)
 	{
 		Scanner kb = new Scanner(System.in);
-		System.out.println("Please enter length of rectangle");
+		System.out.println("Enter the length of rectangle:");
 		l = kb.nextDouble();
-		System.out.println("Please enter width of rectangle");
+		System.out.println("Enter the width rectangle:");
 		w = kb.nextDouble();
-		
-		
+		calcPerim();
+		print();
+	}
+	
+	public static void calcPerim()
+	{
+		perimeter = l + l + w + w;
+	}
+	
+	public static void print()
+	{
+		System.out.println("The perimeter of your rectangle with length " + l + " and width " + w + " is " + perimeter + ".");
 	}
 }
