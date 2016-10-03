f7d3763d32492982ba2f49b11fd53da96092c9d8
rickyheidt <rgthree99@gmail.com>
2016-10-03 11:15:08 -0700

    added files

--- APLesson_04/Sum.java
+++ APLesson_04/Sum.java
@@ -1,12 +1,26 @@
+import java.util.Scanner;
 public class Sum
 {
+	static int one;
+	static int two;
 	public static void main(String[]args)
 	{
-		System.out.println(add(5,5));
+		Scanner kb = new Scanner(System.in);
+		System.out.println("Enter 1st number: ");
+		one = kb.nextInt();
+		System.out.println("Enter 2nd number: ");
+		two = kb.nextInt();
+		add();
+		print();
 	}
 	
-	public static int add(int one, int two)
+	public static int add ()
 	{
 		return one+two;
+	}
+	
+	public static void print()
+	{
+		System.out.println(one + " + " + two + " = " + add());
 	}
 }
