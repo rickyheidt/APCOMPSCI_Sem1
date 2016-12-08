0f26188ee0f7d1b78b87cabe669683d69b13fa29
rickyheidt <rgthree99@gmail.com>
2016-12-08 12:13:15 -0800

    added files

--- APLesson_06/righttri.java
+++ APLesson_06/righttri.java
@@ -1,12 +1,12 @@
 import java.util.Scanner;
-public class lefttri {
+public class righttri {
 	public static void main(String[]args) {
 		Scanner kb = new Scanner(System.in);
 		System.out.println("Please enter a word");
 		String word = kb.next();
 		
 		for(int i = 0; i <=word.length(); i++) {
-			System.out.println(word.substring(i, word.length()));
+			System.out.println(word.substring(word.length(), 0));
 		}
 	}
 }
